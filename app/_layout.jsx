7f8ae88c2abb0cc2db5df4ec0ac68824a606f7a3
Ryan Lee <taiwanesechad@Ryans-MacBook-Pro.local>
2024-04-16 18:34:14 -0500

    Changed font from Poppins to Proxima Nova

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -8,15 +8,10 @@ SplashScreen.preventAutoHideAsync();
 const RootLayout = () => {
 
   const [fontsLoaded, error] = useFonts({
-    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
-    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
-    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
-    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
-    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
-    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
-    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "ProximaNova-Black": require("../assets/fonts/proximanova_black.ttf"),
+    "ProximaNova-Bold": require("../assets/fonts/proximanova_bold.otf"),
+    "ProximaNova-Light": require("../assets/fonts/proximanova_light.otf"),
+    "ProximaNova-Regular": require("../assets/fonts/proximanova_regular.ttf")
   });
 
   useEffect(() => {
