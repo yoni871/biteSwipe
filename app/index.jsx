b92c6971f6f186347af312e1273300c91b88eb37
Ryan Lee <taiwanesechad@Ryans-MacBook-Pro.local>
2024-04-16 18:19:18 -0500

    Added components, assets, and basic file layout

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">BiteSwipe</Text>
+      <Text className="text-3xl font-pblack">BiteSwipe</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
     </View>
